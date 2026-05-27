f5e18d211ec86fdb11ac4016ced54f438deff83b
Saksham Tayal <saksham2447@gmail.com>
2026-05-27 21:45:06 +0530

    204 Count Primes Leetcode

---
+++ 204 Count Primes/204-count-primes-app2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> primes(n+1,true) ;
+        for(int i =2; i*i<n; ++i){
+            if(primes[i] == true){
+                for(int j=i*i ; j<n ; j+=i){
+                    primes[j] = false;
+                }
+            }
+        }
+        int p = 0;
+        for(int i=2 ; i<n; i++){
+            if(primes[i] == true){
+                p++;
+            }
+        }
+        return p;
+    }
+};
