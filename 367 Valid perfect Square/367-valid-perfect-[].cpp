2a0feace6b0d648562ab614a5d913cfe94eb47fb
Saksham Tayal <saksham2447@gmail.com>
2026-05-30 07:40:23 +0530

    Leetcode Que 367 Valid Perfect Square

---
+++ 367 Valid perfect Square/367-valid-perfect-[].cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(long long int i=1; i*i<=num;++i){
+            if(num == i*i){
+                return true;
+            }
+        }
+        return false;
+    }
+};
