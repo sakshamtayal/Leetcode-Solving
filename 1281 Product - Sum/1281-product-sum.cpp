d7272a8e8105ac7083c554b002d9e619047a9d3c
Saksham Tayal <saksham2447@gmail.com>
2026-05-31 11:39:27 +0530

    Leetcode Que 1281 Product - Sum

---
+++ 1281 Product - Sum/1281-product-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int p = 1 , s = 0;
+        while(n > 0){
+            p = p * (n%10) ;
+            s = s + (n%10) ;
+            n = n/10 ;
+        }
+        return p - s ;
+    }
+};
