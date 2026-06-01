3f0a603cc32766c4e13eff5b05d3db66f92e9a14
Saksham Tayal <saksham2447@gmail.com>
2026-06-01 17:56:43 +0530

    Leetcode Que 268

---
+++ 268 Missing Number/268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       if(nums.size() - 1 == nums[nums.size() - 1]){
+        return nums[nums.size()-1] + 1 ;
+       } 
+       for(int i=0;i<nums.size()-1;++i){
+        if(nums[i+1] != nums[i]+1){
+            return nums[i] + 1;
+        }
+       }
+       return 0;
+    }
+};
