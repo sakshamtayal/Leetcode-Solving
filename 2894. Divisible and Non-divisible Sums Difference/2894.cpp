5e14a7a44003804a7d9cfbf6b2c9fc748fc0e6f8
Saksham Tayal <saksham2447@gmail.com>
2026-06-09 08:52:49 +0530

    Leetcode Que 2894

---
+++ 2894. Divisible and Non-divisible Sums Difference/2894.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int a = 0 , b = 0;
+        for(int i=1;i<=n;++i){
+            if(i%m == 0){
+                b += i;
+            }else{
+                a += i;
+            }
+        }
+        return a-b ;
+    }
+};
