1c0183c33883aae8fdceacff3907a2ef4e396a26
Saksham Tayal <saksham2447@gmail.com>
2026-06-09 08:49:54 +0530

    Leetcode Que 1822

---
+++ 1822. Sign of the Product of an Array/1822.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int a = 1;
+        for(int i=0;i<nums.size();++i){
+            if(nums[i] == 0){
+                return 0;
+            } else {
+                if(nums[i] < 0){
+                    a *= -1;
+                }
+            }
+        }
+       return a;
+    }
+};
