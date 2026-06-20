99ab7716ce90812407d5ba957fe779997e91d578
Saksham Tayal <saksham2447@gmail.com>
2026-06-20 08:02:39 +0530

    Leetcode Que 2186

---
+++ 2186. Minimum Number of Steps to Make Two Strings Anagram II/2186.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        vector<int> a(26,0) , b(26,0);
+        for(int i=0;i<s.length();++i){
+            a[s[i]-'a']++;
+        }
+        for(int i=0;i<t.length();++i){
+            b[t[i]-'a']++;
+        }
+        int ans = 0;
+        for(int i=0;i<26;++i){
+            ans += abs(a[i] - b[i]);
+        }
+        return ans;
+    }
+};
