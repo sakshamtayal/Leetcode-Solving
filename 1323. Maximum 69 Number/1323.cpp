b73627bca51c763a96a6944962106909026f09f9
Saksham Tayal <saksham2447@gmail.com>
2026-06-28 23:52:53 +0530

    Leetcode Que 1323

---
+++ 1323. Maximum 69 Number/1323.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int n) {
+        string s = to_string(n);
+        for(int i=0;i<s.length();++i){
+            if(s[i]-'0' == 6){
+                s[i] = '9';
+                break;
+            }
+        }
+        n = stoi(s);
+        return n;
+    }
+};
