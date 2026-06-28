685b5c0ca29599f589d08fc4a52f3912528ee9d6
Saksham Tayal <saksham2447@gmail.com>
2026-06-28 23:56:01 +0530

    Leetcode Que 1342

---
+++ 1342. Number of Steps to Reduce a Number to Zero/1342.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfSteps(int n) {
+        int ans = 0;
+        while(n>0){
+            if(n%2 == 0){
+                n /= 2;
+                ans++;
+            }else{
+                n -= 1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
